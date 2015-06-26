75f3a080b20563a869d56848ba0d29b81afa4b07
Eduardo Moreno <eduardo@Eduardos-MacBook-Pro.local>
2015-06-26 11:28:58 -0700

    cleaned it up a little and added comments to scrape.js

--- scrape.js
+++ scrape.js
@@ -23,13 +23,17 @@ var alchemyapi = new AlchemyAPI();
 var reviewsArray;
 
 var searchUrl = 'http://www.yelp.com/search?find_desc='
-var searchWord = 'pizza'
+var searchWord = 'tacos'
 var searchString = '&find_loc=San+Francisco%2C+CA&ns=1'
 var intId;
 var intId2;
 var locArr = []
 
+
 var reviews = {}
+
+// Finds URLs on Yelp of 10 locations that match the searchWord, stores URLs in locArr[]
+// Calls 'callMany()', intermittently 
 
 function getUrls(searchWord, callback){
 
@@ -53,71 +57,45 @@ function getUrls(searchWord, callback){
 	    // 	requestForALoc(locArr[i]);
 	    // }
 
-	    intId = setInterval(callMany,5000);
+	    intId = setInterval(callMany,3000);
+	  } else {
+	  	console.log(error);
 	  }
 	});
 }
 
-function callback(){
-	// if(reviewsArray)
-		// console.log(reviewsArray)
-	// console.log(counter)
-	if (reviewsArray){
-		// for (var i = 0; reviewsArray.length; i++){
-		// 	// console.log()
-		// 	if(reviewsArray[i]){
-				// console.log(reviewsArray[i]);
-			
-				// var myText = reviewsArray[i]
+// Counts and calls requestForALoc() which requests each individual location page
+// for each location stored in locArr[]. Also, stops intermitten calling of this function
+// once each location has been requested (i.e., counter == locArr.length + 1) 
+// Note: counter starts at 1, because locArr.length == 11 but first index is not a location
 
-				var myText = reviewsArray[counter2]
-				counter2 += 1;
-				
-				if(counter2 == reviewsArray.length + 1)
-					counter2 = 0;
+function callMany(){
 
-				// console.log(myText)
-
-				alchemyapi.sentiment("text", myText, {}, function(response) {
-					// console.log(response)
-					console.log("Sentiment: " + response["docSentiment"]["score"]);
-				});
-			// }
-		// }
-	}
+		// console.log(counter)
+		requestForALoc()	
+		
+		counter += 1
+		if (counter == locArr.length || counter == 12)
+			clearInterval(intId)
 	
 }
 
-
-
-function callMany(){
-	// for (var i = counter; i < 5; i++){
-		console.log(counter)
-		requestForALoc()
-		
-		
-		counter += 1
-		if (counter == 5)
-			clearInterval(intId)
-	// }
-}
+// Makes request to a particular location page and stores its reviews in locReviews[].
+// The reviews (inside of locReviews[]) are then stored in an object ('reviews') along
+// with the URL of the yelp location page from which they were found. The keys for the
+// reviews object are the partial urls of each location, which are each an object that 
+// stores their respective full url and array of reviews (locReviews). Also, this function
+// intermittently calls callback().
 
 function requestForALoc(){
+
 	var url = 'http://www.yelp.com'
 	url += locArr[counter]
 
-	// if(aLoc)
-	// 	console.log(aLoc)
-
 	var locReviews = []
-	// var counter = 1;
-    // var getUrlCallback = function
+
 	request(url, function (error, response, html) {
 
-	  // counter++;
-	  // if (counter === aLoc.length){
-	  // 	callback();
-	  // }
 	  if (!error && response.statusCode == 200) {
 	    var $ = cheerio.load(html);
 	    // console.log(html);
@@ -133,16 +111,40 @@ function requestForALoc(){
 	    reviews[locArr[counter]] = { "url" : url, "locReviews" : locReviews}
 	    reviewsArray = reviews[locArr[counter]]['locReviews'] 
 	    
+	    console.log(reviewsArray.length)
+
 	    intId2 = setInterval(callback, 1000);
 
 	    // console.log(reviews["/biz/little-star-pizza-san-francisco"])
-	  	
-	  	// counter += 1;
 
 	  } else {
 	    	console.log(error)
 	    }
 	});
+}
+
+// Makes request to Alchemy API for sentiment analysis of each review for a location.
+// counter2 tracks the index within reviewsArray. Usually reviewsArray.length == 40 (sometimes 50)
+
+function callback(){
+
+				// stop interval HERE????
+
+				if(counter2 == reviewsArray.length + 1){
+					counter2 = 0;
+					clearInterval(intId2)
+				}
+
+				var myText = reviewsArray[counter2]
+				counter2 += 1;
+				
+				alchemyapi.sentiment("text", myText, {}, function(response) {
+				
+					console.log("Sentiment: " + response["docSentiment"]["score"]);
+
+				});
+	
+	
 }
 
 getUrls(searchWord)
