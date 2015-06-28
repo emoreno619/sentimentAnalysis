52d115733f05ed631502b081d5f10a3e58319b2c
Eduardo Moreno <eduardo@eduardos-mbp.gateway.pace.com>
2015-06-28 00:04:38 -0700

    fixed some front end ux stuff for how search results display and details toggle

--- public/js/google.js
+++ public/js/google.js
@@ -47,10 +47,11 @@ $(function(){
 	  if (status == google.maps.places.PlacesServiceStatus.OK) {
 	    for (var i = 0; i < results.length; i++) {
 	      var place = results[i];
-	      console.log(place.name + " " + place.rating)
-	      console.log(place.types)
-	      console.log(place.formatted_address)
-	      console.log(place.place_id)
+	      // console.log(place.name + " " + place.rating)
+	      
+	      // console.log(place.types)
+	      // console.log(place.formatted_address)
+	      // console.log(place.place_id)
 	      if(place.place_id)
 	      	ids.push({placeId: place.place_id})
 	    
@@ -58,7 +59,7 @@ $(function(){
 	    }
 	  }
 	  detailsRequest();
-	  console.log(ids)
+	  // console.log(ids)
 	}
 
 	function detailsRequest(){
@@ -73,6 +74,7 @@ $(function(){
 	function callback2(place, status) {
 	  if (status == google.maps.places.PlacesServiceStatus.OK) {
 	    console.log(place.name + " " + place.formatted_phone_number)
+	    console.log(place)
 	    createResultsDiv(place);
 	  }
 	}
@@ -80,12 +82,26 @@ $(function(){
 	function createResultsDiv(aResult){
 		if (aResult.formatted_phone_number){
 			var phoneId = aResult.formatted_phone_number.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~() ]/g,"")
-			$('#savedPlacesEnd').append('<div id=' + phoneId + '>' + aResult.name + '</div>')
-			console.log('GOOGLE COORD: lat: ' + aResult.geometry.location.A + ' long: ' + aResult.geometry.location.F)
+			$('#savedPlacesEnd').append('<div id=' + phoneId + '>' + aResult.name + ' <a id=toggle>see more</a></div>')
+			// console.log('GOOGLE COORD: lat: ' + aResult.geometry.location.A + ' long: ' + aResult.geometry.location.F)
 			if (aResult.rating)
-				$('#'+ phoneId).attr('class', 'aPlace ' + aResult.rating)
+				$('#'+ phoneId).attr('class', 'aPlace ' + aResult.rating + ' ' + aResult.user_ratings_total)
 			else
 				$('#'+ phoneId).attr('class', 'aPlace noRating')
+
+			var div = $('#'+ phoneId)
+
+			
+			if(!div.hasClass('noRating')){
+				var classes = div.attr('class').split(" ")
+				
+				div.append("<div id=gRating> Google+ Rating: " + classes[1] + " (" + classes[2] + " reviews)</div>")
+			} else {
+				div.append("<div id=gRating> Google+ Rating: No Rating </div>")
+			}
+
+			ajaxYelpShenanigans(div);
+
 			$('#map-canvas').css('width', '80%').css('float', 'right').css('border', '2px solid black').css('border-radius', '2%')
 			$('#wrapper').css('width', '18%').css('float', 'left')
 			$('#'+ phoneId).css('border', '2px solid black').css('width', '80%').css('margin', '5%').css('float', 'none').css('border-radius', '2%').css('padding-left', '2%')
@@ -97,16 +113,7 @@ $(function(){
 		}
 	}
 
-	$("body").on("click", ".aPlace", function(event){
-		div = $(this)
-		if(!div.has('#gRating').length){
-			if(!div.hasClass('noRating')){
-				var classes = div.attr('class').split(" ")
-				div.append("<div id=gRating> Google+ Rating: " + classes[1] +"</div>")
-			} else {
-				div.append("<div id=gRating> Google+ Rating: No Rating </div>")
-			}
-		}
+	function ajaxYelpShenanigans(div){
 		var toSend = {}
 		
 		var str = div.html()
@@ -127,16 +134,34 @@ $(function(){
 			  // $('#newpuppyform').remove();
 			  // $('#newpuppylink').show();
 			  // loadLocations();
-			  console.log( datas.businesses[0].name + " " + datas.businesses[0].rating + " " + datas.businesses[0].phone)
+			  // console.log( datas.businesses[0].name + " " + datas.businesses[0].rating + " " + datas.businesses[0].phone)
 			  if(!div.has('#yRating').length){
 			  	// if(!div.hasClass('noRating')){
-			  		console.log("YELP COORD " + "Latitude: " + datas.businesses[0].location.coordinate.latitude + " Longitude: " + datas.businesses[0].location.coordinate.longitude)
-			  		div.append("<div id=yRating> Yelp Rating: " + datas.businesses[0].rating +"</div>")
+			  		// console.log("YELP COORD " + "Latitude: " + datas.businesses[0].location.coordinate.latitude + " Longitude: " + datas.businesses[0].location.coordinate.longitude)
+			  		div.append("<div id=yRating> Yelp Rating: " + datas.businesses[0].rating + " (" + datas.businesses[0].review_count + " reviews)" +"</div>")
+			  		div.append('<div id=address style="display: none;">'+ datas.businesses[0].location.address +'</div>')
+			  		div.append('<div id=phoneNum style="display: none;">Phone: '+ datas.businesses[0].phone +'</div>')
 			  	// } else {
 			  	// 	div.append("<div id=gRating> Yelp Rating: No Rating </div>")
 			  	// }
 			  }
 			});
+		}
+	}
+
+	$("body").on("click", ".aPlace", function(event){
+		div = $(this)
+		if(div.children('#toggle').html() == 'see more'){
+			div.children('#yRating').show()
+			div.children('#toggle').html('see less')
+			div.children('#address').show()
+			div.children('#phoneNum').show()
+
+		} else {
+			div.children('#address').hide()
+			div.children('#phoneNum').hide()
+			div.children('#toggle').html('see more')
+			// 	div.html(div.html() + ' <a id=toggle>see more</a>')
 		}
 	})
 
